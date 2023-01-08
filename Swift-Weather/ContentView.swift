1dffbcb167f22f14cc3f52d2d0f92300ce7903ce
acl <aungcholatt888@gmail.com>
2023-01-08 19:52:28 +0630

    Initial Commit

---
+++ Swift-Weather/ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  Swift-Weather
+//
+//  Created by macair on 07/01/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HStack {
+            Text(/*@START_MENU_TOKEN@*/"Placeholder"/*@END_MENU_TOKEN@*/)
+            VStack {
+                VStack {
+                    Image(systemName: "")
+                        .imageScale(.large)
+                        .foregroundColor(.accentColor)
+                    Text("Hello, world!")
+                }
+                .padding()
+                Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
+                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                }
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Swift-Weather/Swift_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_WeatherApp.swift
+//  Swift-Weather
+//
+//  Created by macair on 07/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct Swift_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
